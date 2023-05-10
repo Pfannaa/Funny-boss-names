5eb3a8c3421ccb53bb93263cc58d2ec7c1c8981c
Lukas Pfanner <lukas.pfann@gmail.com>
2023-05-10 21:45:42 +0200

    Init

--- src/main/java/com/funnybossnames/FunnyBossNamesPlugin.java
+++ src/main/java/com/funnybossnames/FunnyBossNamesPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.funnybossnames;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Funny Boss Names"
 )
-public class ExamplePlugin extends Plugin
+public class FunnyBossNamesPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private FunnyBossNamesConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	FunnyBossNamesConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(FunnyBossNamesConfig.class);
 	}
 }
